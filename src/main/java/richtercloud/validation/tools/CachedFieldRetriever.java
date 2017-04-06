992cd248a1e377c59d15744964c4fcc1e6ce855d
Karl-Philipp Richter <krichter722@aol.de>
2017-04-06 22:58:04 +0200

    initial commit

---
+++ src/main/java/richtercloud/validation/tools/CachedFieldRetriever.java
@@ -0,0 +1,94 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package richtercloud.validation.tools;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
+import richtercloud.validation.tools.annotations.Skip;
+
+/**
+ *
+ * @author richter
+ */
+public class CachedFieldRetriever implements FieldRetriever {
+    /**
+     * A cache for return values of {@link #retrieveRelevantFields(java.lang.Class)
+     * }.
+     */
+    private Map<Class<?>, List<Field>> relevantFieldsCache = new HashMap<>();
+
+    /**
+     * Recursively retrieves all fields from the inheritance hierachy of
+     * {@code entityClass}, except {@code static} and {@code transient} fields
+     * and those annotated with {@link Skip}.
+     *
+     * Results are cached in order to ensure that future calls return the same
+     * result for the same argument value.
+     *
+     * @param clazz
+     * @return
+     */
+    @Override
+    public List<Field> retrieveRelevantFields(Class<?> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("clazz mustn't be null");
+        }
+        List<Field> retValueCandidate = this.relevantFieldsCache.get(clazz);
+        if (retValueCandidate != null) {
+            return new LinkedList<>(retValueCandidate); //return a copy in order to avoid ConcurrentModificationException
+        }
+        List<Field> retValue = new LinkedList<>();
+        Class<?> hierarchyPointer = clazz;
+        while (hierarchyPointer != null //Class.getSuperclass returns null for topmost interface
+                && !hierarchyPointer.equals(Object.class)) {
+            retValue.addAll(Arrays.asList(hierarchyPointer.getDeclaredFields()));
+            hierarchyPointer = hierarchyPointer.getSuperclass();
+        }
+        Set<Field> seenEntityClassFields = new HashSet<>();
+        ListIterator<Field> entityClassFieldsIt = retValue.listIterator();
+        while (entityClassFieldsIt.hasNext()) {
+            Field entityClassFieldsNxt = entityClassFieldsIt.next();
+            if (Modifier.isStatic(entityClassFieldsNxt.getModifiers())) {
+                entityClassFieldsIt.remove();
+                continue;
+            }
+            if (Modifier.isTransient(entityClassFieldsNxt.getModifiers())) {
+                entityClassFieldsIt.remove();
+                continue;
+            }
+            if (seenEntityClassFields.contains(entityClassFieldsNxt)) {
+                entityClassFieldsIt.remove();
+                continue;
+            }
+            Skip entityClassFieldNxtSkip = entityClassFieldsNxt.getAnnotation(Skip.class);
+            if(entityClassFieldNxtSkip != null) {
+                entityClassFieldsIt.remove();
+                continue;
+            }
+            seenEntityClassFields.add(entityClassFieldsNxt);
+            entityClassFieldsNxt.setAccessible(true);
+        }
+        this.relevantFieldsCache.put(clazz, retValue);
+        return retValue;
+    }
+}
---
+++ src/main/java/richtercloud/validation/tools/ValidationTools.java
@@ -0,0 +1,108 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package richtercloud.validation.tools;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+
+/**
+ *
+ * @author richter
+ */
+public class ValidationTools {
+
+    /**
+     * Builds a useful message from multiple constraint violations
+     * @param violations the detected constraint violations to build the message
+     * from
+     * @param instance the instance which causes the constaint violation(s)
+     * @param fieldRetriever the field retriever to use to enhance the message
+     * with field information
+     * @return the built message
+     */
+    /*
+    internal implementation notes:
+    - Set<ConstraintViolation<?>> must not be further parameterized because
+    ConstraintViolationException.getConstraintVioloations doesn't allow
+    parameterization and returns Set<ConstraintViolation<?>>
+    */
+    public static String buildConstraintVioloationMessage(Set<ConstraintViolation<?>> violations,
+            Object instance,
+            FieldRetriever fieldRetriever,
+            FieldNameLambda fieldNameLambda) {
+        StringBuilder messageBuilder = new StringBuilder(1000);
+        messageBuilder.append("<html>");
+        messageBuilder.append("The following constraints are violated:<br/>");
+        for(ConstraintViolation<?> violation : violations) {
+            String propertyPath = violation.getPropertyPath().toString();
+            List<String> propertyPathSplit = new LinkedList<>(Arrays.asList(propertyPath.split("\\."))); //an empty string causes a list with "" to be returned by "".split("\\."") (not necessarily intuitive)
+            String fieldName = propertyPath;
+            //there's no way to retrieve information about the field or
+            //class on which the validation annotation has been specified
+            //-> retrieval of this information is done with the property
+            //path of the ConstraintViolation
+
+            //violations which occur at the class level of the root instance
+            //have an empty property path -> just display the message in a
+            //separate line
+            //note: both violations which occur at the root and at
+            //a property have a property path with 1 node (which doesn't
+            //necessarily make sense) -> there's no way to evaluate retrieve
+            //the field names if a nested violation occurs -> split
+            //propertyPath.toString() at `.`.
+            if(!propertyPath.isEmpty()) {
+                if(propertyPathSplit.size() > 1) {
+                    throw new IllegalArgumentException("Property path of violation is larger than 2 nodes. This isn't supported yet.");
+                }
+                if(!propertyPathSplit.isEmpty()) {
+                    //should be always true because it's already checked
+                    //that propertyPath.toString isn't empty, but check
+                    //nevertheless
+                    String violationFieldName = propertyPathSplit.get(0);
+                    Field violationField = null;
+                    List<Field> classFields = fieldRetriever.retrieveRelevantFields(instance.getClass());
+                    for(Field classField : classFields) {
+                        if(classField.getName().equals(violationFieldName)) {
+                            violationField = classField;
+                            break;
+                        }
+                    }
+                    if(violationField == null) {
+                        throw new IllegalArgumentException("validation violoation constraint on field which isn't part of the validated instance");
+                    }
+                    String specialFieldName = fieldNameLambda.getFieldName(violationField);
+                    if(specialFieldName != null) {
+                        fieldName = specialFieldName;
+                    }
+                    messageBuilder.append(fieldName);
+                    messageBuilder.append(": ");
+                }
+            }
+            messageBuilder.append(violation.getMessage());
+            messageBuilder.append("<br/>");
+        }
+        messageBuilder.append("Fix the corresponding values in the components.");
+        messageBuilder.append("</html>");
+        String message = messageBuilder.toString();
+        return message;
+    }
+
+    private ValidationTools() {
+    }
+}
